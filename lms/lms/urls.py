ae1b7bc6b4523fd29d36a135e9e6c26568d42550
Bezmylov <bezmylov@mail.ru>
2025-01-28 13:43:58 +0300

    test2

--- lms/lms/urls.py
+++ lms/lms/urls.py
@@ -1,7 +1,7 @@
 """
 URL configuration for lms project.
 
-The `urlpatterns` list routes URLs to views. For more information please see:
+The `u2rlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
 Examples!!@!:
 Function views
